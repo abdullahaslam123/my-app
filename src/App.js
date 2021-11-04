28eb9aafab2fa6e71dd01936375ae396d5ba6de4
Abdullah Aslam <abdullah.aslam@northbaysolutions.net>
2021-11-04 17:55:39 +0500

    Updated React App

--- src/App.js
+++ src/App.js
@@ -4,25 +4,26 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import * as eccrypto from 'eccrypto';
 require('dotenv').config()
+
 function handleSubmit(event) {
   event.preventDefault();
   console.log(event.target.password.value); 
-  var privateKey = 'fad79dd5a542537d6615ed2198476fbea99748566b251acdbf693214d6ff11df'
-// const convertedPrivateKey = privateKey.toString("hex");
-let PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY;
+ var privateKey = 'fad79dd5a542537d6615ed2198476fbea99748566b251acdbf693214d6ff11df'
+ let PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY;
  const publicKeyBuffer = Buffer.from(PUBLIC_KEY,"hex");
- const privatekeyBuffer = Buffer.from(privateKey,"hex");
 eccrypto
   .encrypt(publicKeyBuffer, Buffer.from(event.target.password.value))
   .then(function (encrypted) {
-    let encryptedMessageHex=encrypted.toString("hex")
-    console.log("Encrypted Message",encryptedMessageHex );
+  let objstr=JSON.stringify(encrypted)
+  let base64Date=Buffer.from(objstr).toString('hex'); // converted to hex
+  console.log("Encrypted Message to HEX",Buffer.from(base64Date).toString('base64'))
+console.log("Encrypted Message",encrypted );
     
-eccrypto.decrypt(privatekeyBuffer, Buffer.from(encryptedMessageHex,"hex"))
-.then(function (plaintext) {
-  console.log("Message to part B:", plaintext.toString());
+// eccrypto.decrypt(privatekeyBuffer, jsonObj)
+// .then(function (plaintext) {
+//   console.log("Message to part B:", plaintext.toString());
 
-});
+// });
 })
 
 }
